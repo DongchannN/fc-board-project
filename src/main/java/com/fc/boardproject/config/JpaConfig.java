5644523cb5170c10ae38691762b3bc58692befff
DongchannN <ldc991104@gmail.com>
2023-06-07 21:21:17 +0900

    #5 - JPA 속성 설정
    - JpaConfig 추가로 Auditing 기능 사용
    - application.propertiies 를 yaml 파일로 변경
    - yaml 이용 구성환경 설정
    - TODO : application.yaml 에 있는 비밀번호 가리기

---
+++ src/main/java/com/fc/boardproject/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.fc.boardproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("chan"); // todo : 스프링 시큐리티로 인증 기능 구현 시 수정 필요.
+    }
+}
